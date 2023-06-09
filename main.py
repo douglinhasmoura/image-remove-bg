926d59866f43282b642b7f87a6d02ad529d6e7ca
douglas.moura <douglas.moura@NT-TI-226>
2023-06-09 14:43:59 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from rembg import remove
+
+app = Flask(__name__)
+
+@app.route('/remove_background', methods=['POST'])
+def remove_background():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image uploaded'}), 400
+
+    image = request.files['image']
+    image_data = image.read()
+
+    try:
+        output = remove(image_data)
+    except Exception as e:
+        return jsonify({'error': 'Failed to process image', 'details': str(e)}), 500
+
+    return output, 200, {'Content-Type': 'image/png'}
+
+if __name__ == '__main__':
+    app.run()
